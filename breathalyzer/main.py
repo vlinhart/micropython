58d6e8d74402e9293e04646ea923246ed93af627
Vladimír Linhart <vladimir.linhart@gmail.com>
2017-04-28 11:24:59 +0200

    better breathalyzer+temp logger

--- breathalyzer/main.py
+++ breathalyzer/main.py
@@ -1,10 +1,12 @@
 from time import sleep
 
-from machine import Pin, ADC
+from machine import Pin, ADC, I2C
 from neopixel import NeoPixel
+import ssd1306
 
-# well it's not 1V for LoLin NodeMCU - it's got voltage divider 3.1V -> 1V
+# well it's not 1V for LoLin NodeMCU/Wemos D1 - it's got voltage divider 3.1V -> 1V
 # see http://www.thalin.se/2015/05/using-adc-on-nodemcu-esp8266.html
+# http://pyladies.cz/v1/s016-micropython/index.html
 # sudo screen  /dev/ttyUSB0 115200
 digital = Pin(12, Pin.IN)  # D6
 attempt = 0
@@ -15,9 +17,16 @@ LEDS = 8
 np = NeoPixel(leds, LEDS)
 
 
+i2c = I2C(sda=Pin(5), scl=Pin(4))
+display = None
+try:
+    display = ssd1306.SSD1306_I2C(128,64,i2c)
+except:
+    pass
+
 def lights_up():
     for one in range(LEDS):
-        np[one] = (40, 180, 40)
+        np[one] = (10, 40, 5)
     np.write()
 
 
@@ -25,14 +34,15 @@ def heat_up(seconds=60):
     detector_base_values = []
     countdown = LEDS - 1
     lights_up()
+    ad_val = adc.read()
     for s in range(seconds):
-        print('init: {} out of {}'.format(s, seconds))
+        print('init: {}/{} AD={}'.format(s, seconds, ad_val))
         if (s + 1) % (seconds // LEDS) == 0:
             np[max(countdown, 0)] = (0, 0, 0)
             np.write()
             countdown -= 1
         if s > seconds - 10:
-            detector_base_values.append(adc.read())
+            detector_base_values.append(ad_val)
 
         sleep(1)
     return min(detector_base_values) + 3  # is the reading of zero ...
@@ -41,11 +51,22 @@ def heat_up(seconds=60):
 def show_lights(value):
     for i in range(LEDS):
         if value - detector_base > one_led_range * i:
-            np[i] = (185, 25, 15)
+            np[i] = (90, 14, 7)
         else:
             np[i] = (0, 0, 0)
     np.write()
 
+
+def display_values():
+    if display is None:
+        return
+    line_height = 12
+    display.fill(0)
+    display.text('Vylytej jako!',15,0)
+    # display.text('D2 t:{:.1f} h:{:.1f}'.format(dht22.temperature(), dht22.humidity()),0, 5 + line_height)
+    display.text('V:{}'.format(adc.read()),0,5 + line_height*3)
+    display.text('GC free:{}'.format(gc.mem_free()),0,5 + line_height*4)
+    display.show()
 
 detector_base = heat_up()
 detector_range = 1024 - detector_base
@@ -55,4 +76,5 @@ while True:
     attempt += 1
     print('seconds:', attempt, 'digital:', digital.value(), 'analog:', adc.read(), 'base:', detector_base)
     show_lights(adc.read())
+    display_values()
     sleep(1)
--- temp_logger/main.py
+++ temp_logger/main.py
@@ -14,12 +14,12 @@ if machine.reset_cause() == machine.DEEPSLEEP_RESET:
 else:
     print('power on or hard reset')
 
-
-dht22_pin = machine.Pin(14, machine.Pin.IN) #5
+dht22_pin = machine.Pin(14, machine.Pin.IN)  # 5
 dht22 = dht.DHT22(dht22_pin)
 
 i2c = machine.I2C(sda=machine.Pin(5), scl=machine.Pin(4))
-display = ssd1306.SSD1306_I2C(128,64,i2c)
+display = ssd1306.SSD1306_I2C(128, 64, i2c)
+
 
 def http_get(url):
     _, _, host, path = url.split('/', 3)
@@ -37,17 +37,17 @@ def http_get(url):
 
 def display_text(text):
     display.fill(0)
-    display.text(text,0,0)
+    display.text(text, 0, 0)
     display.show()
 
 
-def display_values(first_line=('Oh Hi Temp!',20,0)):
+def display_values(first_line=('Oh Hi Temp!', 20, 0)):
     line_height = 12
     display.fill(0)
-    display.text(*first_line)
-    display.text('Temp: {:.1f} C'.format(dht22.temperature()),0, 5 + line_height)
-    display.text('Humi: {:.1f} %'.format(dht22.humidity()),0, 5 + line_height*2)
-    display.text('GC free:{}'.format(gc.mem_free()),0,5 + line_height*3)
+    display.text(first_line[0], first_line[1], first_line[2])
+    display.text('Temp: {:.1f} C'.format(dht22.temperature()), 0, 5 + line_height)
+    display.text('Humi: {:.1f} %'.format(dht22.humidity()), 0, 5 + line_height * 2)
+    display.text('GC free:{}'.format(gc.mem_free()), 0, 5 + line_height * 3)
     display.show()
 
 
@@ -63,16 +63,18 @@ def do_connect():
         display_values(first_line=('{} log to {}'.format(attempt, wname), 0, 0))
         wlan.connect(wname, wpass)
         while not wlan.isconnected():
-            time.sleep_ms(50)
+            time.sleep_ms(100)
             attempt += 1
-    display_values(first_line=('net:{}'.format(wlan.ifconfig())))
+            display_values(first_line=('{} log to {}'.format(attempt, wname), 0, 0))
+    display_values(first_line=('net:{}'.format(wlan.ifconfig()), 0, 0))
     print('network config:', wlan.ifconfig())
+    time.sleep(1)
 
 
 display_text('measuring...')
 time.sleep(1)
 dht22.measure()
-print('t:', dht22.temperature(), 'h:',dht22.humidity())
+print('t:', dht22.temperature(), 'h:', dht22.humidity())
 do_connect()
 display_values()
 http_get('http://api.thingspeak.com/update?api_key=QFII8JXOBUBJ4EVP&field4={}&field5={}'.format(
@@ -80,5 +82,4 @@ http_get('http://api.thingspeak.com/update?api_key=QFII8JXOBUBJ4EVP&field4={}&fi
 
 print('GC alloc:{} free:{}'.format(gc.mem_alloc(), gc.mem_free()))
 gc.collect()
-esp.deepsleep(1000000*60*3)
-
+esp.deepsleep(1000000 * 60 * 3)
